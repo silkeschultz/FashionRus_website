0f868e7d5f5dddb432b02def6172f2d97ac9296c
Silke <sisc0003@stud.kea.dk>
2025-09-27 16:32:40 +0200

    tilføjet alt js og gjort single produkt siden dynamisk

---
+++ js/product.js
@@ -0,0 +1,40 @@
+const productContainer = document.querySelector(".productContainer");
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+fetch(`https://kea-alt-del.dk/t7/api/products/${id}`)
+.then((res)=>res.json()).then((product)=>{
+  /* console.log(product.articletype); */
+  
+  productContainer.innerHTML = `
+  <div class="product-image">
+  <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="rød puma trøje" />
+  </div>
+  <div class="product-info">
+  <h1>Product Information</h1>
+  <ul>
+  <li><strong>Model name</strong>: ${product.productdisplayname}</li>
+  <li><strong>Category</strong>: ${product.category}</li>
+  <li><strong>Season</strong>: ${product.season}</li>
+  </ul>
+  <div class="brand-logo">${product.brandname}</div>
+  <p class="brand-slogan">FOREVER. FASTER. FOREVER. BETTER.</p>
+  </div>
+  <div class="purchase-info">
+  <h2>${product.productdisplayname}</h2>
+  <p class="brand-category">${product.brandname} ︳${product.articletype}</p>
+  <p class="price"> ${product.price},- </p>
+  <div class="size-selector">
+  <label for="size">Choose a size</label>
+  <select id="size" name="size">
+  <option value="S">S</option>
+  <option value="M">M</option>
+  <option value="L">L</option>
+  <option value="XL">XL</option>
+  </select>
+  </div>
+  <button class="add-to-basket">Add to basket</button>
+  </div>
+  `;
+  })
